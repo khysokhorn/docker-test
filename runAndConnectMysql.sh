89c51854bc91bcdc8ddd5ef116cea28c4b0bd936
Sokhorn Khy <sokhornkhy88@gmail.com>
2021-08-10 23:49:27 +0700

    init

---
+++ runAndConnectMysql.sh
@@ -0,0 +1,1 @@
+docker run -dp 3000:3000 -w /app -v "$(pwd):/app" --network todo-app -e MYSQL_HOST=mysql -e MYSQL_USER=root -e MYSQL_PASSWORD=secret -e MYSQL_DB=todos node:12-alpine sh -c "yarn install && yarn run dev"
---
+++ runWithVolume.sh
@@ -0,0 +1,3 @@
+#sudo docker run -dp 3010:3010 getting-start;
+
+docker run -dp 3000:3000 -v todo-dp:/home/sokhorn/Desktop getting-start
